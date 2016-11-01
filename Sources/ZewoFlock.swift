2db49c241fd08ae17bf9afe929eb69a2e7166721
Jake Heiser <jakeheiser1@gmail.com>
2016-11-01 16:42:02 -0700

    Spawn multiple zewo processes

--- Sources/ZewoFlock.swift
+++ Sources/ZewoFlock.swift
@@ -22,10 +22,14 @@ public class StopTask: Task {
     public let hookTimes: [HookTime] = [.before("deploy:link")]
     
     public func run(on server: Server) throws {
-        if let pid = try findServerPid(on: server) {
-            try server.execute("kill -9 \(pid)")
-        } else {
+        let pids = try findServerPids(on: server)
+        guard !pids.isEmpty else {
             print("Zewo not running")
+            return
+        }
+        
+        for pid in pids {
+            try server.execute("kill -9 \(pid)")
         }
     }
 }
@@ -37,7 +41,15 @@ public class StartTask: Task {
     
     public func run(on server: Server) throws {
         print("Starting Zewo")
-        try server.execute("nohup \(Paths.executable) >> \(Config.outputLog) 2>> \(Config.errorLog) &")
+        let coreCount: Int
+        if let coreCountOutput = try server.capture("nproc") {
+            coreCount = Int(coreCountOutput) ?? 1
+        } else {
+            coreCount = 1
+        }
+        for _ in 0 ..< coreCount {
+            try server.execute("nohup \(Paths.executable) >> \(Config.outputLog) 2>> \(Config.errorLog) &")
+        }
         try invoke("zewo:ps")
     }
 }
@@ -47,26 +59,31 @@ public class PsTask: Task {
     public let namespace = zewo
     
     public func run(on server: Server) throws {
-        if let pid = try findServerPid(on: server) {
-            print("Zewo running as process \(pid)")
-        } else {
+        let pids = try findServerPids(on: server)
+        if pids.isEmpty {
             print("Zewo not running")
+        } else {
+            for pid in pids {
+                print("Zewo running as process \(pid)")
+            }
         }
     }
 }
 
-private func findServerPid(on server: Server) throws -> String? {
+private func findServerPids(on server: Server) throws -> [String] {
     guard let processes = try server.capture("ps aux | grep \".build\"") else {
-        return nil
+        return []
     }
+    
+    var pids: [String] = []
     
     let lines = processes.components(separatedBy: "\n")
     for line in lines where !line.contains("grep") {
         let segments = line.components(separatedBy: " ").filter { !$0.isEmpty }
         if segments.count > 1 {
-            return segments[1]
+            pids.append(segments[1])
         }
-        return segments.count > 1 ? segments[1] : nil
     }
-    return nil
+    
+    return pids
 }
