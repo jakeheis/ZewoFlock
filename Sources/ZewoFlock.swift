1f991bf3234e913f7380798f7cee3a69c73e0f1a
Jake Heiser <jakeheiser1@gmail.com>
2016-11-04 10:38:22 -0700

    For now just spawn one zewo service to avoid address already in use error

--- Sources/ZewoFlock.swift
+++ Sources/ZewoFlock.swift
@@ -1,47 +1,9 @@
 import Flock
-import Foundation
 
 public extension Flock {
-    static let Zewo: [Task] = SystemdTasks(provider: VaporSystemd()).createTasks()
+    static let Zewo: [Task] = SystemdTasks(provider: ZewoSystemd()).createTasks()
 }
 
-class VaporSystemd: SystemdProvider {
+class ZewoSystemd: SystemdProvider {
     let name = "Zewo"
-    
-    var serviceFilePath: String {
-        return "/lib/systemd/system/\(namespace)@.service"
-    }
-    
-    func start(on server: Server) throws {
-        try executeForAllProcesses("start", on: server)
-    }
-    
-    func stop(on server: Server) throws {
-        try executeForAllProcesses("stop", on: server)
-    }
-    
-    func restart(on server: Server) throws {
-        try executeForAllProcesses("restart", on: server)
-    }
-    
-    func status(on server: Server) throws {
-        try executeForAllProcesses("status", on: server)
-    }
-    
-    func executeForAllProcesses(_ command: String, on server: Server) throws {
-        let count = try processCount(on: server)
-        for i in 0 ..< count {
-            try server.execute("service \(namespace)@\(i + 1) \(command)")
-        }
-    }
-    
-    func processCount(on server: Server) throws -> Int {
-        if let coreCountOutput = try server.capture("nproc")?.trimmingCharacters(in: .whitespacesAndNewlines),
-            let coreCount = Int(coreCountOutput) {
-            return coreCount
-        } else {
-            return 1
-        }
-    }
-    
 }
