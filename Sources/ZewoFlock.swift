316ba97f3e095b4dc5a730f45c8cdcb9701143fd
Jake Heiser <jakeheiser1@gmail.com>
2016-11-24 10:41:57 -0600

    Trying using supervisord

--- Sources/ZewoFlock.swift
+++ Sources/ZewoFlock.swift
@@ -1,10 +1,29 @@
 import Flock
 
 public extension Flock {
-    static let Zewo: [Task] = SystemdTasks(provider: ZewoSystemd()).createTasks()
+    static let Zewo: [Task] = SupervisordTasks(provider: ZewoSupervisord()).createTasks()
 }
 
-class ZewoSystemd: SystemdProvider {
+class ZewoSupervisord: SupervisordProvider {
     let name = "zewo"
-    let serviceName = "zewo"
+    let programName = "zewo"
+    
+    func confFileContents(for server: Server) -> String {
+        var processCount = 1
+        do {
+            if let processCountString = try server.capture("nproc"), let processCountInt = Int(processCountString) {
+                processCount = processCountInt
+            }
+        } catch {}
+        
+        return [
+            "[program:\(programName)]",
+            "command=\(Paths.executable)",
+            "process_name=%(process_num)s",
+            "numprocs=\(processCount)",
+            "autostart=false",
+            "autorestart=unexpected",
+            ""
+        ].joined(separator: "\n")
+    }
 }
