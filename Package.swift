af1208c8cdde2abd4032bb45e9c287255a704fea
Jake Heiser <jakeheiser1@gmail.com>
2017-07-15 18:08:01 -0500

    Added deprecation notice

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "ZewoFlock",
-    dependencies: [
-        .Package(url: "https://github.com/jakeheis/Flock", majorVersion: 0, minor: 1)
-    ]
-)
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import ZewoFlockTests
-
-XCTMain([
-     testCase(ZewoFlockTests.allTests),
-])
